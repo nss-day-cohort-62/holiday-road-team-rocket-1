b2a1cb9ef6c646b652681f77a7fbac2535509546
Vision Filler <visionfiller@gmail.com>
2023-02-08 15:08:26 -0600

    click event

--- scripts/HolidayRoad.js
+++ scripts/HolidayRoad.js
@@ -1,55 +1,18 @@
-import { getEateries } from "./eateries/EateryProvider.js"
-import { getParks } from "./parks/ParkProvider.js"
-import { getBizarreries } from "./attractions/AttractionProvider.js"
 
-export const itinerary = {
-    nationalParkid: 0,
-    bizarrarieId: 0,
-    eateryId: 0
-}
+import { itineraryForm } from "./Itinerary.js"
+
+
+
+
+
+
+
 
 export const HolidayRoad = () => {
-    return `${selectPark()} ${selectEatery()} ${selectBizarraries()}`
+    return `
+
+        ${itineraryForm()}
+    <div class="itineraryPreview"></div>`
 }
 
-export const selectPark = () => {
-    const parks = getParks()
 
-    let html = `        
-        <select class="teams" name = "parks" id="parks">
-            <option value="0">Choose a park</option>
-            ${parks.map(park => {
-                return `<option value="${park.id}" >${park.fullName}</option>`
-            }).join("")}
-        </select>
-`
-    return html
-}
-
-export const selectEatery = () => {
-    const eateries = getEateries()
-
-    let html = `        
-        <select class="teams" name = "eateries" id="eateries">
-            <option value="0">Choose a eatery</option>
-            ${eateries.map(eatery => {
-                return `<option value="${eatery.id}" >${eatery.businessName}</option>`
-            }).join("")}
-        </select>
-`
-    return html
-}
-
-export const selectBizarraries = () => {
-    const bizarreries = getBizarreries()
-
-    let html = `        
-        <select class="teams" name = "bizarreries" id="bizarreries">
-            <option value="0">Choose a bizararrie</option>
-            ${bizarreries.map(bizararrie => {
-                return `<option value="${bizararrie.id}" >${bizararrie.name}</option>`
-            }).join("")}
-        </select>
-`
-    return html
-}
---
+++ scripts/dataAccess.js
@@ -0,0 +1,19 @@
+
+export const itinerary = {
+    nationalParkId: 0,
+    bizarrerieId: 0,
+    eateryId: 0
+}
+
+
+
+
+export const setParkId = (id) => {
+    itinerary.nationalParkId = id
+}
+export const setEateryId = (id) => {
+    itinerary.eateryId = id
+}
+export const setBizarrerieId = (id) => {
+    itinerary.bizarrerieId = id
+}
