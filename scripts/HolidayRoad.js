9b5da29d8c36e728dfb6021f8eba2c7ceec1786b
Vision Filler <visionfiller@gmail.com>
2023-02-09 10:07:55 -0600

    dropdowns and itineraries

--- scripts/HolidayRoad.js
+++ scripts/HolidayRoad.js
@@ -1,15 +1,29 @@
 
-import { itineraryForm } from "./Itinerary.js"
+import { itineraryForm, savedItinerary } from "./Itinerary.js"
+
+export const itinerary = {
+    nationalParkid: 0,
+    bizarrarieId: 0,
+    eateryId: 0
+}
+
+
+
 
 
 
 
 export const HolidayRoad = () => {
     return `
+    
 
         ${itineraryForm()}
-    <div class="itineraryPreview"></div>
-    <div class="SavedItinerary"></div>`
+    <div class = "itineraryPreviewBox">
+     <h2>Itinerary Preview<h2>
+        <div class="itineraryPreview"></div>
+    </div>
+    <div class = "savedItinerary">${savedItinerary()}</div>`
+    
 }
 
 
